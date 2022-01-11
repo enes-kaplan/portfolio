23d048084903126642b0c7c49439b4d6ce8a0071
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-11 16:43:39 +0300

    Started working on about section

--- src/components/Home/AboutMe.vue
+++ src/components/Home/AboutMe.vue
@@ -1,6 +1,24 @@
 <template>
-	<div id="about" class="h-screen bg-indigo-500">
-		ABOUT ME
+	<div id="about" class="flex flex-col sm:flex-row sm:items-center w-4/5 mx-auto">
+		<!-- #region About -->
+		<div class="flex flex-col gap-6 w-full sm:w-1/2 px-8 items-center">
+			<img src="/birb.jpg" class="w-60 h-60 rounded-full border-2 border-dark dark:border-light transition-all duration-300">
+			<h1 class="text-4xl sm:text-7xl font-semibold sm:font-bold">
+				About Me.
+			</h1>
+			<p class="text-sm font-light sm:text-xl">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+			</p>
+			<p class="text-sm font-light sm:text-xl">
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p class="text-sm font-light sm:text-xl">
+				Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+			</p>
+		</div>
+		<!-- #endregion -->
+		<!-- #region Skills -->
+		<!-- #endregion -->
 	</div>
 </template>
 
