42a61a06c16aa20fbf157caeb40111136289e3d0
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-11 14:16:14 +0300

    Added scrollbar customization to firefox as well

--- src/components/layout/Content.vue
+++ src/components/layout/Content.vue
@@ -1,7 +1,5 @@
 <template>
-	<div>
-		<router-view class="mt-header h-[calc(100vh-5rem)] scroll-pt-headers overflow-y-auto pageScrollbar" />
-	</div>
+	<router-view class="mt-header h-[calc(100vh-5rem)] scroll-pt-headers overflow-y-auto pageScrollbar" />
 </template>
 
 <script setup lang="ts">
@@ -9,28 +7,31 @@
 </script>
 
 <style scoped>
+/** This is for Firefox */
+.pageScrollbar {
+  scrollbar-color: #0369A1 white;
+  scrollbar-width: thin;
+	@apply transition-all duration-300;
+}
 .pageScrollbar::-webkit-scrollbar-track {
-	width: 6px;
-	height: 4px;
 	@apply bg-light;
 }
 .pageScrollbar::-webkit-scrollbar {
-	width: 6px;
+	width: 8px;
 	height: 4px;
 }
 .pageScrollbar::-webkit-scrollbar-thumb {
 	@apply bg-light-accent;
 }
 
+.dark .pageScrollbar {
+  scrollbar-color: #CA8A04 #374151;
+  scrollbar-width: thin;
+}
 .dark .pageScrollbar::-webkit-scrollbar-track {
 	@apply bg-dark;
-}
-.dark .pageScrollbar::-webkit-scrollbar {
-	width: 6px;
-	height: 4px;
 }
 .dark .pageScrollbar::-webkit-scrollbar-thumb {
 	@apply bg-dark-accent;
 }
-
 </style>
