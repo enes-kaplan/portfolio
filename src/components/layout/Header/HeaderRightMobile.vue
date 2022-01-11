7f7a2ccfaf3430f99abdab59a50d9f75ff565c9d
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-11 08:46:36 +0300

    Completed mobile sidemenu

--- src/components/layout/Header/HeaderRightMobile.vue
+++ src/components/layout/Header/HeaderRightMobile.vue
@@ -7,12 +7,27 @@
 		<ViewListIcon class="w-8 h-8" />
 	</button>
 	<div
-		class="sm:hidden fixed right-0 top-0 bottom-0 w-48 px-4 py-8 border-l border-gray-400 bg-light dark:bg-dark transition-transform duration-300"
+		class="sm:hidden fixed right-0 top-0 bottom-0 w-48 px-4 py-8 border-l border-gray-400 bg-light dark:bg-dark transition-all duration-300"
 		:class="{ 'translate-x-0': isMenuOpen, 'translate-x-48': !isMenuOpen }"
 	>
 		<ul class="flex flex-col gap-2">
 			<li class="flex items-center gap-2">
 				<ThemeChanger :current-theme="currentTheme" is-sidemenu @set-theme="setTheme" />
+			</li>
+			<li>
+				<button class="hover:underline" @click="scrollAndCloseMenu('info')">
+					Info
+				</button>
+			</li>
+			<li>
+				<button class="hover:underline" @click="scrollAndCloseMenu('projects')">
+					Projects
+				</button>
+			</li>
+			<li>
+				<button class="hover:underline" @click="scrollAndCloseMenu('contact')">
+					Contact Me
+				</button>
 			</li>
 		</ul>
 	</div>
@@ -21,6 +36,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { ViewListIcon } from '@heroicons/vue/outline'
+import { scrollIntoView } from '@/functions/common'
 import ThemeChanger from './ThemeChanger.vue'
 
 const props = defineProps({
@@ -39,5 +55,9 @@ const setTheme = (theme: string) => {
 const isMenuOpen = ref(false)
 const changeMenu = (isOpen: boolean) => {
 	isMenuOpen.value = isOpen
+}
+const scrollAndCloseMenu = (id: string) => {
+	scrollIntoView(id)
+	changeMenu(false)
 }
 </script>
--- src/components/layout/Header/ThemeChanger.vue
+++ src/components/layout/Header/ThemeChanger.vue
@@ -1,15 +1,15 @@
 <template>
 	<button v-if="currentTheme === 'light'" class="flex items-center gap-2" @click="setTheme('dark')">
-		<SunIcon class="w-8 h-8" :class="{ 'w-6 h-6': isSidemenu }" />
 		<span v-if="isSidemenu">
 			Switch Theme
 		</span>
+		<SunIcon class="w-8 h-8" :class="{ 'w-6 h-6': isSidemenu }" />
 	</button>
 	<button v-else class="flex items-center gap-2" @click="setTheme('light')">
-		<MoonIcon class="w-8 h-8" :class="{ 'w-6 h-6': isSidemenu }" />
 		<span v-if="isSidemenu">
 			Switch Theme
 		</span>
+		<MoonIcon class="w-8 h-8" :class="{ 'w-6 h-6': isSidemenu }" />
 	</button>
 </template>
 
