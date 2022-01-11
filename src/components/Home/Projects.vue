0d4dd84364e39118a48c8e61014cc522705cf1bb
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-11 11:17:44 +0300

    Worked on projects section

--- src/components/Home/Projects.vue
+++ src/components/Home/Projects.vue
@@ -1,9 +1,57 @@
 <template>
-	<div id="projects" class="h-screen bg-emerald-500">
-		PROJECTS
+	<div id="projects" class="flex flex-col items-center gap-12 py-8">
+		<div
+			v-for="(project, i) in projects"
+			:key="i"
+			class="w-1/3 min-w-project rounded overflow-hidden bg-light text-light-font dark:bg-dark dark:text-dark-font border border-dark dark:border-light"
+		>
+			<img :src="project.image" class="w-full max-h-48 object-cover">
+			<h3 class="mt-4 px-8 text-center text-3xl">
+				{{ project.name }}
+			</h3>
+			<p class="mt-2 px-8 text-lg">
+				{{ project.description }}
+			</p>
+			<div class="flex w-full mt-4 border-t border-dark dark:border-light divide-x divide-dark dark:divide-light">
+				<router-link :to="project.demoLink" class="action-link group">
+					<ExternalLinkIcon class="w-6 h-6 group-hover:stroke-light dark:group-hover:stroke-dark" />
+					Demo
+				</router-link>
+				<a v-if="project.githubLink" :href="project.githubLink" class="action-link group">
+					<BeakerIcon class="w-6 h-6 group-hover:stroke-light dark:group-hover:stroke-dark" />
+					GitHub
+				</a>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { ExternalLinkIcon, BeakerIcon } from '@heroicons/vue/outline'
 
+const projects = [
+	{
+		name: 'Project 1',
+		image: '/Project1.jpg',
+		description: 'Demo project to practice Vue 3.',
+		demoLink: '/project1',
+		githubLink: 'https://github.com/enes-kaplan'
+	},
+	{
+		name: 'Project 2',
+		image: '/Project2.jpg',
+		description: 'Demo icon project to showcase the component.',
+		demoLink: '/project2',
+		githubLink: 'https://github.com/enes-kaplan'
+	}
+]
 </script>
+
+<style scoped>
+.min-w-project {
+	min-width: 16rem;
+}
+.action-link {
+	@apply flex-grow flex justify-center items-center gap-2 py-2 hover:bg-dark hover:text-light dark:hover:bg-light dark:hover:text-dark;
+}
+</style>
