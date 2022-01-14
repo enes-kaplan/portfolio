f0ab630e1d4b78f8f193242b87cb06be4d7fa42a
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-14 11:12:43 +0300

    Setting up firebase configuration

---
+++ src/functions/firebase_setup.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCNFOnSVhcMYPtrTGs7QCmNM9nf4CM3o-0",
+  authDomain: "portfolio-69af6.firebaseapp.com",
+  projectId: "portfolio-69af6",
+  storageBucket: "portfolio-69af6.appspot.com",
+  messagingSenderId: "410344053740",
+  appId: "1:410344053740:web:244240728283e7c286fec9",
+  measurementId: "G-D3RZG0VMZ7"
+})
+const auth = getAuth(firebaseApp)
+const db = getFirestore(firebaseApp)
+
+onAuthStateChanged(auth, user => {
+	if (user !== null) {
+		console.log('Logged in!')
+	} else {
+		console.log('No user!')
+	}
+})
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import './index.css'
+import './functions/firebase_setup'
 
 createApp(App)
 .use(router)
