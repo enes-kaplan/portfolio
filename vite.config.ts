4078c75a501e5c0ed3b805a540ce0291ce2a50e3
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-20 16:25:45 +0300

    Added sourcemap to vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	},
+	build: {
+		sourcemap: true
+	}
 })
