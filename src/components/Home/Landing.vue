b47288c440be544f20b266331bc53ee4db0a7782
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-21 11:07:47 +0300

    Worked on the landing section
    - Replaced placeholder text
    - Improved styling

--- src/components/Home/Landing.vue
+++ src/components/Home/Landing.vue
@@ -1,20 +1,23 @@
 <template>
 	<section
 		id="landing"
-		class="flex flex-col-reverse sm:flex-row-reverse justify-around w-4/5 h-content mx-auto"
+		class="flex flex-col-reverse sm:flex-row-reverse justify-around w-4/5 min-h-content mx-auto pt-8 sm:pt-0"
 	>
 		<BeakerIcon
 			class="wh-icon my-auto stroke-light-accent dark:stroke-dark-accent transition-colors duration-300"
 		/>
 		<div class="flex flex-col justify-between max-w-xl my-auto">
 			<div class="flex flex-col gap-6 mt-4">
-				<h1>Hello World!</h1>
-				<h2>Lorem Ipsum Dolor Sit Amet!</h2>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-					do eiusmod tempor incididunt ut labore et dolore magna
-					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				<h1 class="text-light-accent dark:text-dark-accent">
+					Hi there! I'm Enes.
+				</h1>
+				<h2>I'm a software developer who creates websites</h2>
+				<p class="sm:text-2xl sm:leading-relaxed">
+					I'm a passionate computer engineer located in Turkey who
+					strives to deliver web applications that are accessible,
+					maintainable and usable. I like reading 📖, travelling 🗺️
+					and playing videogames 🎮. I also work on side projects at
+					times 🌐.
 				</p>
 			</div>
 			<a
@@ -32,8 +35,8 @@ import { BeakerIcon } from '@heroicons/vue/outline'
 </script>
 
 <style scoped>
-.h-content {
-	height: calc(100vh - 5rem);
+.min-h-content {
+	min-height: calc(100vh - 5rem);
 }
 .wh-icon {
 	min-width: 12rem;
