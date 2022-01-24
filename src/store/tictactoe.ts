607a80ff135011960bdacdd42f02709752fa33e7
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-24 14:43:58 +0300

    Added a getter for possible moves list

--- src/store/tictactoe.ts
+++ src/store/tictactoe.ts
@@ -66,6 +66,17 @@ export const useTictactoeStore = defineStore('tictactoe', {
 		},
 		isGameCompleted(): boolean {
 			return this.hasPlayerWon || this.hasAIWon
+		},
+		getPossibleMoves: state => {
+			let moves = []
+			for (let x = 0; x < 3; x++) {
+				for (let y = 0; y < 3; y++) {
+					if (state.board[x][y] === TileValues.EMPTY) {
+						moves.push({ x, y })
+					}
+				}
+			}
+			return moves
 		}
 	},
 	actions: {
