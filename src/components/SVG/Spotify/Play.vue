a34f2bb350eb1e45f0a9f5f2866c1c1a1a50a2a2
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-01-28 14:30:44 +0300

    Added play button and transition to the box

---
+++ src/components/SVG/Spotify/Play.vue
@@ -0,0 +1,14 @@
+<template>
+	<svg
+		class="fill-current hover:fill-current"
+		height="16"
+		role="img"
+		width="16"
+		viewBox="0 0 24 24"
+		aria-hidden="true"
+	>
+		<polygon points="21.57 12 5.98 3 5.98 21 21.57 12" />
+	</svg>
+</template>
+
+<script setup lang="ts"></script>
--- src/components/Spotify/Content/Box.vue
+++ src/components/Spotify/Content/Box.vue
@@ -1,16 +1,23 @@
 <template>
 	<div
-		class="flex flex-col p-4 bg-spotify-dark hover:bg-spotify-gray rounded cursor-pointer"
+		class="group flex flex-col p-4 bg-spotify-dark transition-all duration-300 ease-in-out hover:bg-spotify-gray rounded cursor-pointer"
 	>
-		<img
-			:src="content.image"
-			class="w-40 h-40"
-			:class="{
-				'rounded-full': content.isCircle,
-				'rounded-lg': !content.isCircle
-			}"
-			alt="Content image"
-		/>
+		<div class="relative w-40 h-40">
+			<img
+				:src="content.image"
+				class="w-full h-full"
+				:class="{
+					'rounded-full': content.isCircle,
+					'rounded-lg': !content.isCircle
+				}"
+				alt="Content image"
+			/>
+			<div
+				class="absolute bottom-0 right-0 flex justify-center items-center w-10 h-10 bg-[#2FBA58] rounded-full p-1 transition-all duration-300 ease-in-out transform hover:scale-110 opacity-0 group-hover:opacity-100 translate-y-4 group-hover:translate-y-0"
+			>
+				<PlaySVG />
+			</div>
+		</div>
 		<span class="text-lg font-semibold mt-6">
 			{{ content.title }}
 		</span>
@@ -21,6 +28,8 @@
 </template>
 
 <script setup lang="ts">
+import PlaySVG from '@/components/SVG/Spotify/Play.vue'
+
 const props = defineProps({
 	content: {
 		type: Object,
