acf3ba88744c66c80c45bb85d1c4e9e068b0bdbf
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-02-01 16:26:22 +0300

    Splitted HeaderMenu into a seperate component

---
+++ src/components/Balance/HeaderMenu.vue
@@ -0,0 +1,55 @@
+<template>
+	<ul
+		class="absolute top-full right-0 flex flex-col w-32 bg-indigo-100 dark:bg-gray-500 border border-indigo-300 dark:border-gray-600 rounded shadow-theme transition-default transform overflow-hidden"
+		:class="{
+			'translate-y-0': showOptions,
+			'translate-y-6 opacity-0': !showOptions
+		}"
+		role="menu"
+		:aria-hidden="showOptions === false"
+	>
+		<li>
+			<button class="btn-option" @click="editItem">
+				<PencilAltIcon class="w-6 h-6" />
+				<span>Edit</span>
+			</button>
+		</li>
+		<li>
+			<button class="btn-option" @click="deleteItem">
+				<TrashIcon class="w-6 h-6" />
+				<span>Delete</span>
+			</button>
+		</li>
+	</ul>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { BalanceItem } from '@/functions/firebase_types'
+import { PencilAltIcon, TrashIcon } from '@heroicons/vue/outline'
+
+const props = defineProps({
+	showOptions: {
+		type: Boolean,
+		required: true
+	},
+	item: {
+		type: Object as PropType<BalanceItem>,
+		required: true
+	}
+})
+const emit = defineEmits(['close'])
+
+const editItem = () => {
+	emit('close')
+}
+const deleteItem = () => {
+	emit('close')
+}
+</script>
+
+<style scoped>
+.btn-option {
+	@apply flex items-center gap-4 w-full p-2 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600;
+}
+</style>
--- src/components/Balance/ItemHeader.vue
+++ src/components/Balance/ItemHeader.vue
@@ -13,28 +13,11 @@
 			>
 				<DotsHorizontalIcon class="w-6 h-6" />
 			</button>
-			<ul
-				class="absolute top-full right-0 flex flex-col w-32 bg-indigo-100 dark:bg-gray-500 border border-indigo-300 dark:border-gray-600 rounded shadow-theme transition-default transform overflow-hidden"
-				:class="{
-					'translate-y-0': showOptions,
-					'translate-y-6 opacity-0': !showOptions
-				}"
-				role="menu"
-				:aria-hidden="showOptions === true"
-			>
-				<li>
-					<button class="btn-option" @click="editItem">
-						<PencilAltIcon class="w-6 h-6" />
-						<span>Edit</span>
-					</button>
-				</li>
-				<li>
-					<button class="btn-option" @click="deleteItem">
-						<TrashIcon class="w-6 h-6" />
-						<span>Delete</span>
-					</button>
-				</li>
-			</ul>
+			<HeaderMenu
+				:item="item"
+				:show-options="showOptions"
+				@close="showOptions = false"
+			/>
 		</div>
 	</div>
 </template>
@@ -43,12 +26,9 @@
 import type { PropType } from 'vue'
 import type { BalanceItem } from '@/functions/firebase_types'
 import { ref } from 'vue'
-import {
-	DotsHorizontalIcon,
-	PencilAltIcon,
-	TrashIcon
-} from '@heroicons/vue/outline'
+import { DotsHorizontalIcon } from '@heroicons/vue/outline'
 import TypeIcon from './TypeIcon.vue'
+import HeaderMenu from './HeaderMenu.vue'
 
 const props = defineProps({
 	item: {
@@ -58,12 +38,4 @@ const props = defineProps({
 })
 
 const showOptions = ref(false)
-const editItem = () => {}
-const deleteItem = () => {}
 </script>
-
-<style scoped>
-.btn-option {
-	@apply flex items-center gap-4 w-full p-2 font-medium hover:bg-indigo-200 dark:hover:bg-gray-600;
-}
-</style>
