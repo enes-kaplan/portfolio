5856b48dc923ad0daf02cb7f847dc3d00cd4f2a0
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-02-01 11:11:12 +0300

    Added income/expenditure/balance info on months

--- src/components/Balance/MonthBox.vue
+++ src/components/Balance/MonthBox.vue
@@ -1,16 +1,44 @@
 <template>
 	<router-link
 		:to="monthLink"
-		class="w-64 h-64 p-2 border-2 border-dark dark:border-light rounded"
+		class="w-96 h-48 p-2 border-2 border-dark dark:border-light rounded"
 	>
 		<div
-			class="w-full text-center text-xl font-semibold border-b border-dark dark:border-light"
+			class="w-full text-center text-xl font-semibold border-b-2 border-dark dark:border-light"
 			:class="{
 				'text-light-accent dark:text-dark-accent': isCurrentMonth
 			}"
 		>
 			{{ getMonthName(props.month) }}
 		</div>
+		<ul class="flex flex-col mt-2 p-4">
+			<li class="flex justify-between">
+				<div
+					class="text-emerald-700 dark:text-emerald-500 transition-default"
+				>
+					Income
+				</div>
+				<div
+					class="text-emerald-700 dark:text-emerald-500 transition-default"
+				>
+					{{ summary.TotalIncome.toFixed(2) }}
+				</div>
+			</li>
+			<li
+				class="flex justify-between pb-3 border-b border-dark dark:border-light"
+			>
+				<div class="text-red-700 dark:text-red-500 transition-default">
+					Expenditure
+				</div>
+				<div class="text-red-700 dark:text-red-500 transition-default">
+					{{ summary.TotalExpenditure.toFixed(2) }}
+				</div>
+			</li>
+			<li class="flex justify-between mt-3">
+				<div class="font-semibold">Balance</div>
+				<div class="font-semibold">{{ summaryBalance.toFixed(2) }}</div>
+			</li>
+		</ul>
 	</router-link>
 </template>
 
@@ -18,6 +46,8 @@
 import { computed } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useBalanceStore } from '@/store/balance'
+import { Balance } from '@/functions/firebase_types'
+import { auth } from '@/functions/firebase_setup'
 
 const props = defineProps({
 	month: {
@@ -35,5 +65,23 @@ const monthLink = computed(
 const isCurrentMonth = computed(() => {
 	const currentDate = new Date()
 	return currentDate.getMonth() + 1 === props.month
+})
+const summary = computed(() => {
+	const summ = getSummary(props.month)
+	if (summ) {
+		return summ
+	}
+	const defaultBalance: Balance = {
+		UserId: auth!.currentUser!.uid,
+		Year: selectedYear.value,
+		Month: props.month,
+		TotalExpenditure: 0,
+		TotalIncome: 0,
+		isDeleted: false
+	}
+	return defaultBalance
+})
+const summaryBalance = computed(() => {
+	return summary.value.TotalIncome - summary.value.TotalExpenditure
 })
 </script>
--- src/pages/Balance.vue
+++ src/pages/Balance.vue
@@ -1,5 +1,8 @@
 <template>
-	<AuthorizedContent @authorized="getSummariesOfYear(selectedYear)">
+	<AuthorizedContent
+		class="pb-24"
+		@authorized="getSummariesOfYear(selectedYear)"
+	>
 		<h1 class="text-3xl sm:text-5xl text-center my-4">
 			💵 Keep track of your income and expenditures 💵
 		</h1>
