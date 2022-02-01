bf81fb7af03eeea329a77ffc3ced52d836220acd
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-02-01 16:47:00 +0300

    Added info to ItemBox

--- src/components/Balance/ItemBox.vue
+++ src/components/Balance/ItemBox.vue
@@ -3,12 +3,35 @@
 		class="flex flex-col w-96 h-48 p-2 border-2 border-dark dark:border-light rounded"
 	>
 		<ItemHeader :item="item" />
+		<ul class="flex flex-col mt-2 p-4">
+			<li class="flex justify-between">
+				<div>Type</div>
+				<div
+					class="transition-default"
+					:class="{
+						'text-red-700 dark:text-red-500': isExpenditure(
+							item.Type
+						),
+						'text-emerald-700 dark:text-emerald-500':
+							!isExpenditure(item.Type)
+					}"
+				>
+					{{ itemText }}
+				</div>
+			</li>
+			<li class="flex justify-between">
+				<div>Amount</div>
+				<div>{{ item.Amount }}</div>
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script setup lang="ts">
 import type { PropType } from 'vue'
 import type { BalanceItem } from '@/functions/firebase_types'
+import { computed } from 'vue'
+import { isExpenditure } from '@/functions/firebase_balance'
 import ItemHeader from './ItemHeader.vue'
 
 const props = defineProps({
@@ -17,4 +40,8 @@ const props = defineProps({
 		required: true
 	}
 })
+
+const itemText = computed(() =>
+	isExpenditure(props.item.Type) ? 'Expense' : 'Income'
+)
 </script>
