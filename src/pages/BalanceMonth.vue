9a71365dcc7d42a83d483ee537d1c43ec00e3c63
Enes Kaplan <enes.kaplan@tubitak.gov.tr>
2022-02-02 08:47:54 +0300

    Added year/month values to month page

--- src/pages/BalanceMonth.vue
+++ src/pages/BalanceMonth.vue
@@ -1,12 +1,14 @@
 <template>
 	<AuthorizedContent @authorized="getItems">
-		<div class="flex flex-wrap gap-12 w-full p-24">
-			<ItemTable :items="itemsOfMonth" />
+		<div class="flex justify-center my-8 font-semibold text-2xl">
+			{{ monthName }} {{ year }}
 		</div>
+		<ItemTable :items="itemsOfMonth" />
 	</AuthorizedContent>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useBalanceStore } from '@/store/balance'
 import { storeToRefs } from 'pinia'
@@ -15,13 +17,19 @@ import ItemTable from '@/components/Balance/ItemTable.vue'
 
 const router = useRouter()
 const store = useBalanceStore()
-const { getItemsOfMonth } = store
+const { getMonthName, getItemsOfMonth } = store
 const { itemsOfMonth } = storeToRefs(store)
 
+const monthName = ref('')
+const year = ref('')
 const getItems = () => {
 	const routeParams = router.currentRoute.value.params
-	const month = parseInt(<string>routeParams.month)
-	const year = parseInt(<string>routeParams.year)
-	getItemsOfMonth(year, month)
+	const monthParam = parseInt(<string>routeParams.month)
+	const yearParam = parseInt(<string>routeParams.year)
+
+	monthName.value = getMonthName(monthParam)
+	year.value = yearParam.toString()
+
+	getItemsOfMonth(yearParam, monthParam)
 }
 </script>
